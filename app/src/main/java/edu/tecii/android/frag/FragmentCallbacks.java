bebf9f16f0393f64311823bbcdc4d3655920e4e1
JuanChacon <virgo_gt@hotmail.com>
2016-10-30 22:13:25 -0700

    Initial commit

---
+++ app/src/main/java/edu/tecii/android/frag/FragmentCallbacks.java
@@ -0,0 +1,9 @@
+package edu.tecii.android.frag;
+
+/**
+ * Created by Juan Chacon Holguin on 30/10/2016.
+ */
+public interface FragmentCallbacks {
+    public void onMsgFromMainToFragment(String strValue);
+}
+
---
+++ app/src/main/java/edu/tecii/android/frag/MainCallbacks.java
@@ -0,0 +1,9 @@
+package edu.tecii.android.frag;
+
+/**
+ * Created by Juan Chacon Holguin on 30/10/2016.
+ */
+public interface MainCallbacks {
+    public void onMsgFromFragToMain (String sender,String strValue);
+}
+
